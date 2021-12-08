88d722133cb5ba478e78bcf31a262ce6a4ce8f04
Leandro de Oliveira <leandro.teodoro.oliveira@hotmail.com>
2021-12-08 08:46:10 -0300

    Aprofundando sobre components React

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import './HelloWords.css'
+
+function HelloWorld(){
+    return(
+        <div className="primeiraDiv">
+            <h2 className="primeiraFrase">Minha Frase!</h2>
+        </div>
+    )
+}
+
+
+export default HelloWorld;
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props){
+    return(
+        <div>
+            <p>O nome do elemento é {props.nome}!</p>
+        </div>
+    )
+}
+
+export default SayMyName
