b1fdbf0b6f345daba43d3fc70d2123a603411ddf
Leandro de Oliveira <leandro.teodoro.oliveira@hotmail.com>
2021-12-08 13:10:05 -0300

    Definindo tipos para as props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,24 @@
-function Item(props){
+import PropTypes from 'prop-types'
+
+function Item({marca, modelo, ano_fabricacao}){
     return(
         <>
-            <li>{props.marca}</li>
+            <li>
+                {marca} - {modelo} - {ano_fabricacao}
+            </li>
         </>
 
     )
+}
+
+Item.protoType = {
+    marca: PropTypes.string.isRequired,
+    ano_fabricacao: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Marca não definida',
+    ano_fabricacao: 0,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -8,14 +8,16 @@ function List(){
         <>
             <h1>Minha Lista de carros!</h1>
             <ul>
-                <p>O que eu gosto</p>
-                <Item marca= {cars[0]}/>
-                <p>O que eu tenho</p>
-                <Item marca= {cars[1]}/>
-                <p>Respeito mas não quero ter</p>
-                <Item marca= {cars[2]}/>
+                <p>O que eu gosto:</p>
+                <Item marca= {cars[0]} modelo={320} ano_fabricacao={2021}/>
+                <p>O que eu tenho:</p>
+                <Item marca= {cars[1]} modelo="Gol" ano_fabricacao={2007}/>
+                <p>Respeito mas não quero ter:</p>
+                <Item marca= {cars[2]} modelo="Uno" ano_fabricacao={2000}/>
                 <p>Ouço dizer que tem a melhor mecanica!</p>
-                <Item marca= {cars[3]}/>
+                <Item marca= {cars[3]} modelo="Corola" ano_fabricacao={2018}/>
+                <p>Lista com itens em branco</p>
+                <Item/>
             </ul>
         </>
     )
